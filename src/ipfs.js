c611529031323ac12745123ecc4d2cbef055765d
nguyen huy hoang <hoangnh@meup68.vn>
2019-03-08 10:41:43 +0700

    using https protocol

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,9 +1,9 @@
 const IPFS = require('ipfs-http-client');
-// const ipfs = new IPFS({
-//   host: 'ipfs.infura.io', 
-//   port: 5001, 
-//   protocol: 'https'
-// });
-const ipfs = new IPFS({host: '127.0.0.1', port: 5001, protocol: 'http'});
+const ipfs = new IPFS({
+  host: 'ipfs.infura.io', 
+  port: 5001, 
+  protocol: 'https'
+});
+// const ipfs = new IPFS({host: '127.0.0.1', port: 5001, protocol: 'http'});
 
 export default ipfs;
